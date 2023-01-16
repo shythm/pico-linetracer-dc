38526a8ce242b41a987d4fe53d98e8ff60e94552
Seongho Lee <tjdgh2626@gmail.com>
2023-01-16 23:19:12 +0900

    Test voltage sensing with periodic timer

--- src/main.c
+++ src/main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
-
 #include "pico/stdlib.h"
 
 #include "sensing.h"
+#include "timer.h"
+
+int i = 0;
+
+void sensing_handler(void) {
+  sensing_voltage();
+  printf("%d - voltage: %f \r\n", i, voltage);
+  i++;
+  if (i == 16) {
+    timer_periodic_stop(2);
+  }
+}
 
 int main(void) {
-    stdio_init_all();
+  stdio_init_all();
+  sensing_init();
 
-    sensing_init();
+  timer_periodic_start(2, 500000, sensing_handler);
 
-    for (;;) {
-        printf("voltage: %f \r\n", sensing_voltage());
-        sleep_ms(1000);
-    }
+  for (;;) {
+    printf("Main loop \n", voltage);
+    sleep_ms(1000);
+  }
 
-    return 0;
+  return 0;
 }
