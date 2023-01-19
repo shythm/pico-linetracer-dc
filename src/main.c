bb7eb4a7319a297c327a4866188dcfaa15375bd7
Seongho Lee <tjdgh2626@gmail.com>
2023-01-19 21:59:23 +0900

    Add feature to change motor input voltage using switch

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "switch.h"
 #include "sensing.h"
 #include "timer.h"
 #include "motor_dc.h"
@@ -11,19 +12,35 @@ void sensing_handler(void) {
 
 int main(void) {
   stdio_init_all();
+  switch_init();
   sensing_init();
   motor_dc_init();
 
   timer_periodic_start(2, 500, sensing_handler);
 
-  float input_voltage_left = -12.0f;
-  float input_voltage_right = 12.0f;
-  motor_dc_set_enabled(MOTOR_DC_LEFT, true);
-  motor_dc_set_enabled(MOTOR_DC_RIGHT, true);
+  float input_voltage = 0.f;
+  bool motor_enabled = false;
 
   for (;;) {
-    motor_dc_input_voltage(MOTOR_DC_LEFT,  input_voltage_left, voltage);
-    motor_dc_input_voltage(MOTOR_DC_RIGHT, input_voltage_right, voltage);
+    uint sw = switch_read();
+
+    if (sw == SWITCH_EVENT_LEFT) {
+      input_voltage -= 1.f;
+    } else if (sw == SWITCH_EVENT_RIGHT) {
+      input_voltage += 1.f;
+    } else if (sw == SWITCH_EVENT_BOTH) {
+      motor_enabled = !motor_enabled;
+      motor_dc_set_enabled(MOTOR_DC_LEFT, motor_enabled);
+      motor_dc_set_enabled(MOTOR_DC_RIGHT, motor_enabled);
+    }
+
+    if (motor_enabled) {
+      motor_dc_input_voltage(MOTOR_DC_LEFT,  input_voltage, voltage);
+      motor_dc_input_voltage(MOTOR_DC_RIGHT, input_voltage, voltage);
+      printf("input_voltage: %2.1f, current_voltage: %2.1f \r\n", input_voltage, voltage);
+    } else {
+      printf("motor disabled \r\n");
+    }
   }
 
   return 0;
